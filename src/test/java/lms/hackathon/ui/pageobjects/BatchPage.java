69ec9501a2ef0663090acd8da5636464cbc05243
DivyaChakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-04-28 11:20:20 -0400

    Initial Commit

---
+++ src/test/java/lms/hackathon/ui/pageobjects/BatchPage.java
@@ -0,0 +1,13 @@
+package lms.hackathon.ui.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class BatchPage {
+
+	public WebDriver driver;
+
+	public BatchPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
